dbac25c6e3618ae2398ee3ff7dc17592d1274717
Aman Sharma <amansharma.11@hotmail.com>
2019-06-27 17:58:18 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fragment_lifecycle_trace_events_with_logcat_android_github/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.fragment_lifecycle_trace_events_with_logcat_android_github;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
